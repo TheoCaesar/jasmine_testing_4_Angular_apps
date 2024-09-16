803c07ea2656aaea9b950feb6dcea9d77c74acec
TheoCaesar <theocaesar7@gmail.com>
2024-09-16 14:16:50 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'jasmine_testing_4_Angular_apps';
+}
